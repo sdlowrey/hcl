771d3cc9021f4515e488e1adceb915c2132f82ee
Scott Lowrey <sdlowrey@gmail.com>
2013-05-22 14:59:04 -0400

    Rename 'certifications' app to the singular form

--- hardview/certification/admin.py
+++ hardview/certification/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from certifications.models import Certification
+from certification.models import Certification
 
 admin.site.register(Certification)
--- hardview/hardview/settings.py
+++ hardview/hardview/settings.py
@@ -127,7 +127,7 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     'system',
     'software',
-    'certifications',
+    'certification',
 )
 
 # A sample logging configuration. The only tangible logging
