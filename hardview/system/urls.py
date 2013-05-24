97acd0d3de7842182780b6847b1ad52c294733fd
Scott Lowrey <sdlowrey@gmail.com>
2013-05-24 17:40:31 -0400

    Partially broken: get system list by vendor name.
    This works if you pass in /system/vendor/HP (case sensitive) but the
    idea of creating a link in the overall system list fails.  In fact,
    just accessing /system causes an IndexError.  I don't fully understand
    dynamic filtering in class-based views; this might be a 1.5 concept.

--- hardview/system/urls.py
+++ hardview/system/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 from system.views import SystemList
 
 urlpatterns = patterns('',
-    (r'^$', SystemList.as_view())
+    (r'^$', SystemList.as_view()),
+    (r'^vendor/([\w-]+)$', SystemList.as_view())
 )
